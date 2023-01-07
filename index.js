a6ffaa199877951799fa42bb9593c95ef77d1415
sheblair <66489366+sheblair@users.noreply.github.com>
2023-01-07 14:37:11 -0500

    added dark mode toggle

--- index.js
+++ index.js
@@ -17,3 +17,30 @@ const copyright = document.querySelector(".copyright");
 const currentYear = new Date().getFullYear()
 
 copyright.innerText = `hand-coded by sheila blair ©${currentYear}`;
+
+// dark mode toggle
+const toggle = document.querySelector('.theme-switch');
+const body = document.querySelector("body");
+const links = document.querySelectorAll("a")
+
+
+toggle.addEventListener('input', () => {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light")
+    body.classList.add("dark")
+  } else {
+    body.classList.remove("dark")
+    body.classList.add("light")
+  }
+
+  links.forEach((a) => {
+    if (a.classList.contains("dark-link")) {
+      a.classList.remove("dark-link")
+      a.classList.add("light-link")
+    } else {
+      a.classList.remove("light-link")
+      a.classList.add("dark-link")
+    }
+  })
+});
+
